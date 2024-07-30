88d1b18298d8474ba34d5b102f3cb52802ff7dd4
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2024-07-30 06:15:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/current-time", () => Results.Ok(DateTime.UtcNow));
+
+app.Run();
